e117c411aa2453b1793bf7a7a3700d58a41a7d20
LythAre <71510960+LythAre@users.noreply.github.com>
2024-05-17 16:08:24 +0300

    x

--- html5game/test_project.js
+++ html5game/test_project.js
@@ -80,23 +80,24 @@ function gml_Object_Object2_Step_0(_inst,_other){
 	}
 		
 	if (window.cloudStorage) {
-		window.cloudStorage
-		.set('my-key', 'my-value1')
-		.then(() => console.log('Item saved'));
+		// window.cloudStorage
+		// .set('my-key', 'my-value1')
+		// .then(() => console.log('Item saved'));
 
-		window.cloudStorage
-		.get('my-key')
-		.then((value) => {
-			console.log(value);
-			a = value;
-		});
+		// window.cloudStorage
+		// .get('my-key')
+		// .then((value) => {
+		// 	console.log(value);
+		// 	a = value;
+		// });
 
-		if (a == 'my-value1') {
-			Telegram.WebApp.sendData(JSON.stringify(25565));
-		}
+		// if (a == 'my-value1') {
+		// 	Telegram.WebApp.sendData(JSON.stringify(25565));
+		// }
 	}
 	else {
-		global.gmlhighscore = 55555
+		console.log(55555555555)
+		//global.gmlhighscore = 55555
 	}
 }
 function gml_Object_Object2_Draw_64(_inst,_other){draw_set_font(YYASSET_REF(0x06000000));draw_text(yyfdivide(__yy_gml_errCheck(g_pBuiltIn.room_width),
