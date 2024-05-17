b7fbca650dd8ecceaca3c4340b6de04b4b806197
LythAre <71510960+LythAre@users.noreply.github.com>
2024-05-17 19:22:16 +0300

    s

--- html5game/test_project.js
+++ html5game/test_project.js
@@ -1,8 +1,4 @@
-﻿var script1 = document.createElement('script');
-	script1.src = 'https://telegram.org/js/telegram-web-app.js';
-	document.body.appendChild(script1);
-
-t = 0;
+﻿t = 0;
 i = 5;
 unk = 0;
 
