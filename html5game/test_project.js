beadbed825a3fb1a8a0e28c431c4752a721f0fd9
LythAre <71510960+LythAre@users.noreply.github.com>
2024-05-17 13:53:17 +0300

    b

--- html5game/test_project.js
+++ html5game/test_project.js
@@ -2,12 +2,12 @@
 	script1.src = 'https://telegram.org/js/telegram-web-app.js';
 	document.body.appendChild(script1);
 var script2 = document.createElement('script');
-	script2.src = 'https://cdn.jsdelivr.net/npm/@tma.js/sdk@2.0.2/+esm';
+	script2.src = 'https://cdn.jsdelivr.net/npm/@tma.js/sdk@2.0.2/dist/index.js';
 	script2.type = "module";
 	document.body.appendChild(script2);
 
 a = '0';
-import { CloudStorage, postEvent } from '/npm/@tma.js/sdk@2.0.2/dist/index.js';
+//import { CloudStorage, postEvent } from '/npm/@tma.js/sdk@2.0.2/dist/index.js';
 
 script2.onload = function() {
 	
