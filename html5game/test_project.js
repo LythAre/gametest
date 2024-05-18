b875c772ad7e42e0157f1e62f3f49c465b4f0e4f
LythAre <71510960+LythAre@users.noreply.github.com>
2024-05-18 22:48:48 +0300

    z

--- html5game/test_project.js
+++ html5game/test_project.js
@@ -59,6 +59,7 @@ function gml_Object_Object2_Step_0(_inst,_other){
 		// 	window.cloudStorage.set('highscore', String(global.gmlhighscore));
 		// }
 		else if (closingBehaviour) {
+			closingBehaviour.enableConfirmation();
 			closingBehaviour.on('change:isConfirmationNeeded', (isConfirmationNeeded) => {
 				// Если подтверждение закрытия не требуется
 				if (!isConfirmationNeeded) {
